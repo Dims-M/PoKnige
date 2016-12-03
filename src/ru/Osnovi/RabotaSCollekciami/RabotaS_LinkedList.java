1fc2c65aedd4caec572feb12e470cec0a29c6be1
Dims_M <dimsxm@yandex.ru>
2016-12-04 02:12:48 +0300

    Разбираем Линкен лист

---
+++ src/ru/Osnovi/RabotaSCollekciami/RabotaS_LinkedList.java
@@ -0,0 +1,27 @@
+package ru.Osnovi.RabotaSCollekciami;
+
+import java.util.List;
+
+/**
+ * Created by User on 004 04.12.16.
+ */
+public class RabotaS_LinkedList {
+
+    public static void main(String[] args) {
+
+
+
+    } // конец майна
+
+    public  static void VividLista(List list){
+        System.out.println("Вывод содержимого ");
+
+        for (Object str : list){
+            list.add(str);
+        }
+
+    }
+
+
+
+} // конец класса
