316206ef1faac8d503cf3dfa55c2f93cc70435ed
Dims_M <dimsxm@yandex.ru>
2016-12-06 21:44:41 +0300

    Комит versia 0.00002 из 0.00001 Разбираем Линкен лист

--- src/ru/Osnovi/RabotaSCollekciami/RabotaS_LinkedList.java
+++ src/ru/Osnovi/RabotaSCollekciami/RabotaS_LinkedList.java
@@ -1,5 +1,6 @@
 package ru.Osnovi.RabotaSCollekciami;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,23 +10,63 @@ public class RabotaS_LinkedList {
 
     public static void main(String[] args) {
 
+        String [] zveri ={"кот","пингвин","волк","ежигг","дракон"};
+        String [] zveri1 ={"удав","волк","ЕжиК","Ниблнр"};
+
+        // линкин лист для хранения связанных списков
+        LinkedList listZveri = new LinkedList<>();
+        List<String> listZveri2 = new LinkedList<>();
+
+        zapolnenieLista(zveri, (LinkedList) listZveri);
+        vividRazdelitela();
+        zapolnenieLista(zveri1, (LinkedList) listZveri2);
+        vividLista((LinkedList) listZveri);
+        System.out.println("Длинна Листа = "+ counntlist((LinkedList) listZveri));
+        vividRazdelitela();
+        vividLista((LinkedList)listZveri2);
+        System.out.println("Длинна Листа = "+ counntlist((LinkedList) listZveri2));
+        soedinenie(listZveri, (LinkedList) listZveri2);
+        vividRazdelitela();
+        vividLista(listZveri);
+        System.out.println("Длинна Листа = "+ counntlist ((LinkedList) listZveri));
+
+
+
 
 
     } // конец майна
 
-    public  static void zapolnenieLista(List list){
+    public  static LinkedList zapolnenieLista(String [] str , LinkedList list){
         System.out.println("Вывод содержимого ");
-
-        for (Object str : list){
-            list.add(str);
+        int count =0;
+        for (String stroki : str){
+            list.add(stroki);
         }
+
+            return list;
+
     }
 
-    public static void vividLista (List list){
+    public static void vividLista (LinkedList list){
         for (Object str: list){
             System.out.println(str);
         }
     }
 
+    public static LinkedList soedinenie (LinkedList list, LinkedList list2){
+
+        list.addAll(list2);
+
+        return list;
+
+    }
+
+    public static int counntlist (LinkedList list){
+        return list.size();
+    }
+
+    public static void vividRazdelitela(){
+        System.out.println("*******************");
+    }
 
 } // конец класса
