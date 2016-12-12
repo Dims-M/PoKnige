e22717ec37a005ba94af9cbe31d90aed859caf4a
Dims_M <dimsxm@yandex.ru>
2016-12-12 22:36:33 +0300

    Комит versia 0.00007 из 0.00001 Разбираем Линкен лист Аррей лист немного Колекционс Изучение Stack<String> stack = new Stack<String>();

--- src/com/rus/VsakoRazno/Stek.java
+++ src/com/rus/VsakoRazno/Stek.java
@@ -1,15 +1,29 @@
 package com.rus.VsakoRazno;
 
+import java.util.Stack;
+
 /**
  * Created by User on 010 10.12.16.
  */
 public class Stek {
-    private static Stek ourInstance = new Stek();
+    public static void main(String[] args) {
 
-    public static Stek getInstance() {
-        return ourInstance;
-    }
+        // Создаем обьект стэк типа Стринг
+        Stack<String> stack = new Stack<String>();
+        //Метод пуш позволяет добавить что то в стек
+        stack.push("Первонах");
 
-    private Stek() {
-    }
-}
+        //****************
+        System.out.println("***************");
+        System.out.println("Текущий размер Стека равен = "+stack.size());
+        System.out.println("***************");
+        System.out.println("Содежимое Стека = "+stack.peek());
+        stack.peek();
+
+    } // конец метода майн
+
+
+
+
+} // конец класса
+
