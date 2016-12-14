f428412eff521cb679ac2e09ade00cd23f18b0f9
Dims_M <dimsxm@yandex.ru>
2016-12-15 01:55:14 +0300

    Комит versia 0.00007 из 0.00001 Разбираем Линкен лист Аррей лист немного Колекционс Изучение Stack<String> stack = new Stack<String>(); Stek and Queue

---
+++ src/com/rus/VsakoRazno/Rabota_S_Queue.java
@@ -0,0 +1,18 @@
+package com.rus.VsakoRazno;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Rabota_S_Queue {
+
+    public static void main(String[] args) {
+
+        System.out.println("Разбираем колекцию Очередь. Или Queue");
+
+        //Создаем новый обьект типа PriorityQueue
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+
+
+    } // конец методв майн
+
+}
--- src/com/rus/VsakoRazno/Stek.java
+++ src/com/rus/VsakoRazno/Stek.java
@@ -1,5 +1,6 @@
 package com.rus.VsakoRazno;
 
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -21,17 +22,39 @@ public class Stek {
         System.out.println("***************");
         System.out.println("Содежимое Стека = "+stack.peek());
         stack.peek();
+
         // получение данных по индексу
         System.out.println(stack.get(0));
+        System.out.println(stack.get(1));
+        System.out.println(stack.get(2));
+
+        // Вывод содержимого стека через метод
+        printStekcs(stack);
         // Убрать данные из стека Послежний добавденый стек!!!!!
         stack.pop();
+       // stack.pop();
+       // stack.pop();
 
-
-
+        printStekcs(stack);
 
     } // конец метода майн
 
 
+    public static void printStekcs (Stack<String> stack){
+        System.out.println("Выводим содержимое стека!!");
+
+        if (stack.empty()){
+            System.out.println("Стек пуст. Нет ни каких значений");
+        }
+
+        else {
+            for (String string : stack) {
+                System.out.println(string);
+            }
+            System.out.println("Длина стека = " + stack.size());
+        }
+        System.out.println("Вывод завершен");
+    }
 
 
 } // конец класса
