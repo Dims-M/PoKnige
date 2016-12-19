33380b4cde1b1fdf5a6492bb6026d2a6d65ea90e
Dims_M <dimsxm@yandex.ru>
2016-12-19 22:47:57 +0300

    Комит versia 0.00008 из 0.00001               Разбираем Линкен лист Аррей лист немного Колекционс               Изучение Stack<String> stack = new Stack<String>();               Stek and Queue               Set, HashSet

--- src/com/rus/VsakoRazno/Rabota_S_Queue.java
+++ src/com/rus/VsakoRazno/Rabota_S_Queue.java
@@ -12,7 +12,30 @@ public class Rabota_S_Queue {
         //Создаем новый обьект типа PriorityQueue
         PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
 
+        // Что бы добавить (Нужные данные) в очередь используем команду offer
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+
+        // Вывод на консоль с помощью форматирования текста
+        System.out.printf("%s", queue);
+        System.out.println("Размер текущей очереди = "+ queue.size());
+
+        // Самый первый элемент который находится в очереди имеет высшию приорететность
+        //вывод приоритета c помощью команты peek
+        System.out.println("******");
+
+        System.out.println("Тукущий приоритет = "+queue.peek());
+        System.out.println("******");
+        // после удаление приоритета следущий за ним тип данных становится приоритетом
+        // Удаление приоритета
+        queue.poll();
+        System.out.printf("%s", queue);
+        System.out.println("Размер текущей очереди после удаления приоритета = "+ queue.size());
+        System.out.println("******");
+        System.out.println("Тукущий приоритет = "+queue.peek());
+
+
 
     } // конец методв майн
-
 }
---
+++ src/com/rus/VsakoRazno/Rabota_S_Set_HashSet.java
@@ -0,0 +1,7 @@
+package com.rus.VsakoRazno;
+
+public class Rabota_S_Set_HashSet {
+
+
+
+}
