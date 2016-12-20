5b5ee6f8597e273e1fe5384ca14dc70388725749
Dims_M <dimsxm@yandex.ru>
2016-12-21 00:12:00 +0300

    Комит versia 0.000010 из 0.00001                                         Разбираем Линкен лист Аррей лист немного Колекционс                                         Изучение Stack<String> stack = new Stack<String>();                                         Stek and Queue                                         Set, HashSet                                         Знакомство с Generic                                         Знакомство с Аплетами

--- src/com/rus/VsakoRazno/Rabota_S_Appletes.java
+++ src/com/rus/VsakoRazno/Rabota_S_Appletes.java
@@ -1,15 +1,25 @@
 package com.rus.VsakoRazno;
 
 import  java.awt.*;
+import  javax.swing.*;
 /**
  * Created by User on 020 20.12.16.
  */
-public class Rabota_S_Appletes {
+// указываем чта наш класс наследуется от extends JApplet
+public class Rabota_S_Appletes extends JApplet {
 
-    public static void main(String[] args) {
+    // создали класс для вывода(Рисования) графического окна
+    public void paint (Graphics g){
+        // вызываем метод paint из ролдителького класса с помощью команды super с параметром g
+        super.paint(g);
+        g.drawString("Это мой первый Аплет",300,400);
+
+}
+
+   //public static void main(String[] args) {
         String infa =  "Аплеты используют работу java в вашем интернет браузере";
 
-    } // конец метода майн
+  //  } // конец метода майн
 
 
 } // конец класса
