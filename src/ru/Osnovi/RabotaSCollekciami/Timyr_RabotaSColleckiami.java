db41e5c6c5d40d69235ab7a36537c8c92ed2cb0c
Dims_M <dimsxm@yandex.ru>
2017-01-08 22:14:19 +0300

    Комит versia 0.000014 из 0.00001                                         Разбираем Линкен лист Аррей лист немного Колекционс                                         Изучение Stack<String> stack = new Stack<String>();                                         Stek and Queue                                         Set, HashSet                                         Знакомство с Generic                                         Знакомство с Аплетами                                         Начало ищучения Иквелся Тостринга и хешкода

---
+++ src/ru/Osnovi/RabotaSCollekciami/Timyr_RabotaSColleckiami.java
@@ -0,0 +1,27 @@
+package ru.Osnovi.RabotaSCollekciami;
+
+/**
+ * Created by User on 008 08.01.17.
+ */
+public class Timyr_RabotaSColleckiami {
+
+    public static void main(String[] args) {
+
+        String collekcion = "Джава колекцион фрамворк (JCF) - часто называют набор классов и нтерфейсов в JAVA " +
+                "для работы с коллекциями ";
+
+     String collekcia = "Это группировка(иногда называют контейнер обьектов определенного типа, например каллекция товаров магазина ";
+
+     String infaOkolekcii = "Существуют готовые коллекции под различные задачи(Очередь, Связанный список, индексированый список и др) ";
+
+     String primenenie = "Часе всего применяются для хранения различтных обьектов ";
+
+     String primenenie2 = "Многие фреймоворки ORM Основаны на колекциях (Hibernate? jpa ) ";
+
+
+
+
+    } // Конец майла
+
+
+}
