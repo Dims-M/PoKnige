ad932f2b15d6a18fb6deafcd86cb2970be0ac34f
Dims_M <dimsxm@yandex.ru>
2017-01-29 01:17:06 +0300

    Комит versia 0.000016 из 0.00001                                         Разбираем Линкен лист Аррей лист немного Колекционс                                         Изучение Stack<String> stack = new Stack<String>();                                         Stek and Queue                                         Set, HashSet                                         Знакомство с Generic                                         Знакомство с Аплетами                                         Начало ищучения Иквелся Тостринга и хешкода                                         Добавление метода для выисления среднего значения                                         СОЗДАНИЕ игры САПЕР

---
+++ src/com/rus/VsakoRazno/ru/Game/Saper/GameMiners.java
@@ -0,0 +1,36 @@
+package com.rus.VsakoRazno.ru.Game.Saper;
+
+import javax.swing.*;
+
+/**
+ * Created by User on 029 29.01.17.
+ */
+public class GameMiners extends JFrame {
+
+    //Работаем с переменными
+
+    // Имя окна програмы
+    final String TITLE_OF_PROGRAMM = "Минёрр";
+    // Символ отображения тревоги.
+    final  String SING_OF_flag = "F";
+    //Размер блока в пикселях
+    final int BLOCK_SIZE = 30;
+    // Размер поля в квадратах
+    final int FIELD_SIZE = 9;
+    //координаты
+    final int FIELD_DX = 6;
+    final int FIELD_DY = 28;
+    // Нахождение курсора при начале игры/ от ВЕРХНЕГО ЛЕВОГО ОКНА
+    final int START_LOCATION = 200 ;
+    // Константы овечают(возращаею) при нажатии кнопки мыши. Обработчики событий
+    final int MOUSE_BUTTON_LEFT = 1;
+    final int MOUSE_BUTTON_RIGHT = 3;
+    public static void main(String[] args) {
+
+
+    } // конец метода майн
+
+
+
+}
+
