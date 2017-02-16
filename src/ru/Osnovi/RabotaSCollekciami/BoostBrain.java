f9db04b3a0051a8d4de7711ea0eb19731471c142
Dims_M <dimsxm@yandex.ru>
2017-02-16 23:16:19 +0300

    Комит versia 0.000016 из 0.00001                                         Разбираем Линкен лист Аррей лист немного Колекционс                                         Изучение Stack<String> stack = new Stack<String>();                                         Stek and Queue                                         Set, HashSet                                         Знакомство с Generic                                         Знакомство с Аплетами                                         Начало ищучения Иквелся Тостринга и хешкода                                         Добавление метода для выисления среднего значения                                         СОЗДАНИЕ игры САПЕР

--- src/ru/Osnovi/RabotaSCollekciami/BoostBrain.java
+++ src/ru/Osnovi/RabotaSCollekciami/BoostBrain.java
@@ -9,20 +9,32 @@ import java.io.IOException;
  */
 public class BoostBrain {
 
-    public static void main(String[] args) {
+  static   String strokaIstochnika = "D:/123.txt";
+  static   String strokaIstochnika1 = "D:/moscow-buildings.CSV";
 
+    public static void main(String[] args) throws IOException {
 
+        chenieFaila (strokaIstochnika);
+        chenieFaila (strokaIstochnika1);
 
     }
 
-    public static void chenieFaila () throws IOException {
+    public static void chenieFaila (String istochnik) throws IOException {
 
-        FileInputStream stream = new FileInputStream("D:/123.tht");
+        FileInputStream stream = new FileInputStream(istochnik);
         // создали обьект для открытия этого файла.
 
         // теперь его надо считать из файла
         // узнаем и сохраняем длину потока(считанного файла)
         int length = stream.available();
+
+        // маччив дл хрвнения битовых значенй
+        byte [] data = new byte[length];
+        stream.read(data);
+
+        String text = new String(data);
+
+        System.out.println(text);
 
 
     }
