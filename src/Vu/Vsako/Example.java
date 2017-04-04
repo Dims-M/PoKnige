f129c656b424d9b8edd77e23859ec92d4e93a6fd
Dims_M <dimsxm@yandex.ru>
2017-04-04 22:02:14 +0300

    Комит versia 0.000016 из 0.00001                                         Разбираем Линкен лист Аррей лист немного Колекционс                                         Изучение Stack<String> stack = new Stack<String>();                                         Stek and Queue                                         Set, HashSet                                         Знакомство с Generic                                         Знакомство с Аплетами                                         Начало ищучения Иквелся Тостринга и хешкода                                         Добавление метода для выисления среднего значения                                         СОЗДАНИЕ игры САПЕР

--- src/Vu/Vsako/Example.java
+++ src/Vu/Vsako/Example.java
@@ -1,11 +1,52 @@
 package Vu.Vsako;
 
+import java.util.Scanner;
+
 /**
  * Created by User on 006 06.02.17.
  */
 public class Example {
 
     public static void main(String[] args) {
+        srednieZnachenie ();
+    }
+
+    public static void srednieZnachenie (){
+        Scanner vklad_S = new Scanner(System.in);
+        Scanner vrema_S = new Scanner(System.in);
+        Scanner prosent_S = new Scanner(System.in);
+
+        System.out.println("Программа для подсчета процентов по вкладу!");
+        System.out.println("*******************************************");
+
+        System.out.println("Введите сумму вклада и нажмите Ентр");
+        double tempVklad = vklad_S.nextDouble();
+        System.out.println("Введите срок вашего влада. В полных месяцах");
+        int tempVrema = vrema_S.nextInt();
+        System.out.println("Введите процентную ставку");
+        double tempProsent = prosent_S.nextDouble();
+
+       // tempVrema *= 30;
+        double itogPoVkladu = 0;
+        for (int i = 1;i<=tempVrema;i++){
+             itogPoVkladu += tempVklad*tempProsent;
+        }
+
+       // itogPoVkladu = (tempVklad * tempProsent) + tempVrema;
+
+       // double itogPoProsentu = (tempVklad *tempProsent * (tempVrema / 360)) /100;
+        double itogPoProsentu = tempProsent * tempVrema;
+        // double itogPoProsentu = tempVklad * (1  + tempProsent*30/365/100) 6 - tempVklad ;
+
+        System.out.println("Производится расчет......\n");
+        System.out.println("------------------------------");
+        System.out.println("Ваш вклад = "+tempVklad +"\n"+
+                           "Ваши процентная ставка = "+ tempProsent+"\n"+
+                           "Ваше время вклада = "+tempVrema + " \n");
+
+        System.out.println("Ваши насчитанные проценты по вкладу = "+ itogPoProsentu);
+        System.out.println("Ваша общая сумма по вкладу = "+ itogPoVkladu);
+
 
     }
 
